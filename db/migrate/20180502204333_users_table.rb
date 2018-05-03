095de29bc5dbf57682c7e8859d91593f712097d3
Aaron Masih <aaron@Ashers-MacBook-Pro.local>
2018-05-03 16:38:13 -0400

    first commit

---
+++ db/migrate/20180502204333_users_table.rb
@@ -0,0 +1,13 @@
+class UsersTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :firstname
+      t.string :lastname
+      t.string :bday
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+end
